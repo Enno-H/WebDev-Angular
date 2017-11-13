356e1de39e245875a6144cedcb71a278dd28ee39
Xiaolong Huang <xiaolong_huang@foxmail.com>
2017-11-13 16:34:16 +0800

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { HttpModule } from '@angular/http';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
