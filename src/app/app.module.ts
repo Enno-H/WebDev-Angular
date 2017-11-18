9a884a2e3398adb159597205abf34c682345e55c
Xiaolong Huang <xiaolong_huang@foxmail.com>
2017-11-18 22:08:14 +0800

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service'
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
