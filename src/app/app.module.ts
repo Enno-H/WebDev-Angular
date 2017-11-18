e38d5e18b1c67ddbe226517b1e8e42788fbdc9a3
Xiaolong Huang <xiaolong_huang@foxmail.com>
2017-11-18 19:38:44 +0800

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service'
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
