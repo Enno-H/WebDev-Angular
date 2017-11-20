fdc57f88f36645c242672663e223bec9be2d1e35
Xiaolong Huang <xiaolong_huang@foxmail.com>
2017-11-20 18:58:45 +0800

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component'
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -41,7 +43,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService,PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
